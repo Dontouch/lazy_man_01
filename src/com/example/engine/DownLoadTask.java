316526d692904320d028a5f6073ced40289abb3f
Dontouch <972759558@qq.com>
2016-01-09 01:03:16 +0800

    01
    引入库

---
+++ src/com/example/engine/DownLoadTask.java
@@ -0,0 +1,17 @@
+package com.example.engine;
+
+/**
+ * 
+ * обть╫ЬЁл
+ * @author Dontouch
+ *
+ */
+public class DownLoadTask implements Runnable{
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/com/example/lazy_man_01/WelcomeActivity.java
@@ -0,0 +1,10 @@
+package com.example.lazy_man_01;
+
+/**
+ * ª∂”≠ΩÁ√Ê
+ * @author Dontouch
+ *
+ */
+public class WelcomeActivity {
+
+}
